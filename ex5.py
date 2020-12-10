40c9cf8dfc4051c57d231b47461982ac3aeb49ff
Aniket Anand <aniket3108@gmail.com>
2020-12-10 13:08:34 +0530

    Divide Func

--- ex5.py
+++ ex5.py
@@ -11,7 +11,7 @@ p2k=0.45
 print(f"Lets talk about {name}.")
 print(f"He's {height*i2c} centimeters tall.")
 print(f"He's {weight*p2k} kilograms heavy.")
-print("Actually that's not too heavy or is it.")
+print("Actually that's not too much heavy or is it.")
 print(f"He's got {eyes} eyes and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on the coffee.")
 
