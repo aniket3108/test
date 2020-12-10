8da1d605b2ac9f29a2eb94a2f89b9081ea213023
aniket3108 <62802084+aniket3108@users.noreply.github.com>
2020-12-10 12:06:35 +0530

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,20 @@
+name = 'Aniket Anand'
+age = 32 # not a lie
+height = 69 # inches
+weight = 152 # lbs
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
+i2c= 2.54
+p2k=0.45
+
+print(f"Lets talk about {name}.")
+print(f"He's {height*i2c} centimeters tall.")
+print(f"He's {weight*p2k} kilograms heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+#This line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height} and {weight} I get {total}")
---
+++ ex7.py
@@ -0,0 +1,38 @@
+# Printing a string
+print("Mary had a little lamb.")
+# printing formated string, string inside string passed using format
+print("It's fleece was white as {}.".format('snow'))
+# printing ag stri
+print("And everywhere that Mary went.")
+# Printing . 10 times.
+print("."*10) # What'd that do?
+# Assigngqwing end1 variable with a value of C
+end1 = "C"
+# Assigning end2 variable with a value of h
+end2 = "h"
+# Assigning end3 variable with a value of e
+end3 = "e"
+# Assigning end4 variable with a value of e
+end4 = "e"
+# Assigning end5 variable with a value of s
+end5 = "s"
+# Assigning end6 qevariable with a value of e
+end6 = "e"
+# Assigning end7 variable with a value of B
+end7 = "B"
+# Assignin ;wevj;g end8 variable with a value of u
+end8 = "u"
+# Assiqwfqwfgning end9 variable with a value of r
+end9 = "r"
+# Assigning end10 variable with a value of g
+end10 = "g"
+# Assigning end11 variable with a value of e
+end11 = "e"
+# Assigning end12 variable with a value of r
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what h
+# printing concatenated value of (end1, end2, end3, end4, end5, end6)
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ')# end= ' ', what it does is instead of new line, next statements would start in the same line
+# prwqinting concatenated value of (end7, end8, end9, end10, end11, end12)
+print(end7 + end8 + end9 + end10 + end11 + end12)
